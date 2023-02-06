1d699fc25a4c8c2aaadb22e355414d6af23a984b
Ansari Azaz Ahmad <121128619+Ajaz7267@users.noreply.github.com>
2023-02-06 21:08:48 +0530

    UC1

--- AddressBook.java
+++ AddressBook.java
@@ -1,7 +1,33 @@
 package AddressBook;
 
 public class AddressBook {
-    public static void main(String[] args) {
-        System.out.println("Welcome To Address Book Program");
+    ArrayList<AddressBook> people = new ArrayList<AddressBook>();
+    Scanner sc = new Scanner(System.in);
+    String firstName, lastName, address, city, state, zip, phoneNumber, email;
+    AddressBook person = new AddressBook();
+
+    public static void main(String args[]) {
+    AddressBook ad= new AddressBook();
+    ad.AddContact();
+
     }
+        public void AddContact () {
+            System.out.println("enter first name");
+            person.firstName = sc.nextLine();
+            System.out.println("enter last name");
+            person.lastName = sc.nextLine();
+            System.out.println("enter address");
+            person.address = sc.nextLine();
+            System.out.println("enter city name");
+            person.city = sc.nextLine();
+            System.out.println("enter state");
+            person.state = sc.nextLine();
+            System.out.println("enter zip code");
+            person.zip = sc.nextLine();
+            System.out.println("enter phone number");
+            person.phoneNumber = sc.nextLine();
+            System.out.println("enter email address");
+            person.email = sc.nextLine();
+            people.add(person);
+        }
 }
